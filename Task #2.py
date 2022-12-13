f836d2ab9ea6a5d8bfb54031e0b32096e6b7bebf
dvsni <danil.shir@outlook.com>
2022-12-13 20:38:12 +0300

    Task2

--- Task #2.py
+++ Task #2.py
@@ -1,2 +1,19 @@
 # Напишите программу для проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def defenition(x,y,z):
+    if(not(x or y or z)) == ((not x) and (not y) and (not z)):
+        return True
+    else:
+        return False
+print('Истинность утверждения ¬(X V Y V Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
+print('X Y Z - result')
+string_input = "1000"
+for i in range (1, 9):
+    print(string_input[1], ' ', string_input[2], ' ', string_input[3], '-', defenition(bool(string_input[1]), bool(string_input[2]), bool(string_input[3])))
+    j = str(bin(i))
+    j = int(j[2:5])
+    string_input=int(1000+j)
+    string_input=str(string_input)
+
+    
