54afb49fa5ce7e70c3d6a8776e82764a37294ace
dvsni <danil.shir@outlook.com>
2022-12-16 15:46:01 +0300

    Task2.4

--- Task #2.4.py
+++ Task #2.4.py
@@ -1,3 +1,16 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 # Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
+
+with open('List.txt','r') as f:
+    a = f.read().split('\n')
+a = list(map(int, a))
+n = int(input('Введите число: '))
+gen = [i for i in range(-n, n+1)]
+kn = 1
+for i in a:
+    kn *= gen[i]
+    print(kn)
+print(a)
+print(gen)
+print(kn)
