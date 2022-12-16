c698f4d901a2cffe8b9dab8f442189999a489955
dvsni <danil.shir@outlook.com>
2022-12-16 15:37:37 +0300

    Task2.2

--- Task #2.2.py
+++ Task #2.2.py
@@ -2,3 +2,10 @@
 # *Пример:*
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+number_n = int(input("Введите целое число: "))
+factorial_list = [1]
+i = 1
+while i < number_n:
+    factorial_list.append(factorial_list[i - 1]*(i + 1))
+    i+=1
+print(factorial_list)
