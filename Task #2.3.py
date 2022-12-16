7563ab9c0270218bee8c5d22616f6a3e87c27c26
dvsni <danil.shir@outlook.com>
2022-12-16 15:38:26 +0300

    Task2.3

--- Task #2.3.py
+++ Task #2.3.py
@@ -2,4 +2,17 @@
 # *Пример:*
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-
+def f(n):
+    result = []
+    for i in range(1, n + 1):
+        result.append((1+1/i)**i)
+    return result
+n = int(input('Введите целое число N: '))
+print(f(n))
+result = f(n)
+def sum_list(result):
+    sum = 0
+    for i in range(len(result)):
+        sum += result[i]
+    return sum
+print(f'Сумма = {sum_list(result)}')
