9b9bf1a73d19b8d5f29c0bca0454712eb1930872
dvsni <danil.shir@outlook.com>
2022-12-16 15:39:01 +0300

    Task2.5

--- Task #2.5.py
+++ Task #2.5.py
@@ -1,2 +1,18 @@
 # Реализуйте алгоритм перемешивания списка.
 
+import random
+def mix_list(list_mix):
+     list = list_mix[:]
+     list_length = len(list)
+     for i in range(list_length):
+         index_unknow = random.randint(0, list_length-1)
+         temp = list[i]
+         list[i] = list[index_unknow]
+         list[index_unknow] = temp
+     return list
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+mixed_list = mix_list(list)
+print("Исходный список: ")
+print(list)
+print("Список в случайном порядке: ")
+print(mixed_list)
