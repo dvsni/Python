1f988208ef706858636ee198ea3d1ff646fc0163
dvsni <danil.shir@outlook.com>
2022-12-20 00:06:50 +0300

    Task 3.4

--- Task #3.4.py
+++ Task #3.4.py
@@ -3,3 +3,15 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+import math
+def to_bin(input_decimal, i):
+    if (input_decimal < 0):
+        return "Не преобразуется"
+    elif (input_decimal > 0):
+        temp = input_decimal % 2
+        return to_bin(input_decimal//2, i + 1) + int(temp*math.pow(10,i))
+    else:
+        return 0
+decimal_number = int(input("Введите десятичное число: "))
+print("Преобразование десятичного числа в двоичное: ", to_bin(decimal_number, 0))
