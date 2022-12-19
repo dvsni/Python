014f2d9e766318c4f007aeebaababa02555ab9f4
dvsni <danil.shir@outlook.com>
2022-12-19 23:59:57 +0300

    Task 3.2

--- Task #3.2.py
+++ Task #3.2.py
@@ -3,3 +3,29 @@
 # *Пример:*
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+from random import randint as rnd
+def randomize(input_list, qtys):
+    i = 0
+    qtys = int(qtys)
+    input_list = []
+    while i < qtys:
+        input_list.append(rnd(0, 10))
+        i += 1
+    return input_list
+
+def mult_elem (input_list):
+    i=0
+    multiplied_list = []
+    while i<=len(input_list)%2+1:
+        multiplied_list.append(input_list[i]*input_list[len(input_list)-i-1])
+        i+=1
+    return multiplied_list
+
+qty = input('Введите количество элементов массива: ')
+pure_list = []
+pure_list = randomize(pure_list,qty)
+print(pure_list)
+print("Перемноженные элементы массива -> ",(mult_elem(pure_list)))
+
+
