c11b6913cacfd201e4b890ad5966499e389d7a0d
dvsni <danil.shir@outlook.com>
2023-01-17 13:50:37 +0300

    Task #6.1

--- Task #6.1.py
+++ Task #6.1.py
@@ -2,7 +2,15 @@
 # состоящий из элементов последовательности 3n + 1
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
+n = int(input('Введите число N: '))
+my_dict = {}
+for i in range(1, n+1):
+    my_dict[i] = 3*i + 1
+print(my_dict)
+
+# Улучшение кода:
+
 n = int(input('Введите число: '))
-d = [((3*i) + 1) for i in range(n + 1)]
+d = [((3*i) + 1) for i in range(1, n + 1)]
 d = dict(enumerate(d))
 print(d)
---
+++ Task #6.2.py
@@ -0,0 +1,7 @@
+# # Задайте список из нескольких чисел. 
+# # Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции/
+
+# from typing import List
+
+# def count_sum(num_list: List[int]):
+#     return sum([num for i, num in enumerate(num_list) if i % 2])
