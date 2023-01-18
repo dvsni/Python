eef85b7af581fd6304ee1f350d7eb29505c57e43
dvsni <danil.shir@outlook.com>
2023-01-18 12:49:17 +0300

    Task #6.1.2

--- Task #6.1.2.py
+++ Task #6.1.2.py
@@ -4,3 +4,13 @@
 # 100
 # out
 # [20, 21, 40, 42, 60, 63, 80, 84, 100]
+
+num = int(input("Введите число N: "))
+
+def uniq_list(num):
+    i = 20
+    result_list = [i for i in range(i, num + 1) if i <= num and i % 20 == 0 or i % 21 == 0]
+    return result_list
+
+res = uniq_list(num)
+print(res)
